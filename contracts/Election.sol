baefa0b684aab113b99c2c64adb8abf8762f9615
waititu <waititurobbin@gmail.com>
2020-03-26 22:39:59 +0300

    1. Smoke Test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.4.11;
+pragma solidity >=0.4.21 <0.7.0;
 
 contract Election{
     // Store candidate
     // Read candidate
     string public candidate;
     // Constructor
-    function Election () public {
+    function Election1 () public {
       candidate = "Candidate 1";
     }
 }
