f85372422cfd8ffb8e2ffee2c57e92b112a61342
waititu <waititurobbin@gmail.com>
2020-03-26 22:00:17 +0300

    create smart contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.11;
 contract Election{
     // Store candidate
     // Read candidate
+    string public candidate;
     // Constructor
     function Election () public {
       candidate = "Candidate 1";
