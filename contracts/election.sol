fdff2c2bf8b5aa4d6d76cac5fdb157aaa2297465
waititu <waititurobbin@gmail.com>
2020-03-26 20:26:06 +0300

    create base of the dapp

---
+++ contracts/election.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+contract Election{
+    // Store candidate
+    // Read candidate
+    // Constructor
+    function Election () public {
+      candidate = "Candidate 1";
+    }
+}
